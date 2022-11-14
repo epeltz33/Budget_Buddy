fa2a8df95ca52a5e8d81f402b8fbf2305900f834
Eric-Peltzman33 <erpeltz@gmail.com>
2022-11-13 21:09:48 -0500

    connected the db finally

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_migrate import Migrate
-from models import db, InfoModel
+from models import db, User
 
 app = Flask( __name__ )
 
 app.config[ 'SQLALCHEMY_DATABASE_URI' ] = "postgresql://ericpeltzman:Theboys36@localhost:5432/budget_buddy" #
 app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS' ] = False
-
-
 db.init_app( app )
 migrate = Migrate( app, db )
 
@@ -15,24 +13,21 @@ migrate = Migrate( app, db )
 def form():
     return render_template( 'form.html' )
 
-
-@app.route( '/login', methods = [ 'POST', 'GET' ])
-def login():
-    if request.method == 'GET':
-        return "Please submit the form instead."
-    elif request.method == 'POST':
-        form_data = request.form
-        name = form_data[ 'name' ]
-        email = form_data[ 'email' ]
+@app.route( '/submit' , methods = [ 'POST' ] )
+def submit():
+    if request.method == 'POST':
+        name = request.form[ 'name' ]
+        email = request.form[ 'email' ]
         print( name, email )
-        info = InfoModel( name = name, email = email )
-        db.session.add( info )
+        data = User( name, email )
+        db.session.add( data )
         db.session.commit()
-        return "Form submitted!"
-     
+        return redirect( url = 'http://localhost:5000/form' )
     
+@app.route( '/users' ) 
+def users():
+    users = User.query.all()
+    return render_template( 'users.html' , users = users )
 
-     
 if __name__ == '__main__':
-    app.run(debug=True)   
-
+    app.run( debug = True )
--- models.py
+++ models.py
@@ -1,27 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db = SQLAlchemy() 
 
-class InfoModel( db.Model ):
-    __tablename__ = 'info'
-    id = db.Column( db.Integer, primary_key = True )
-    name = db.Column( db.String( 80 ) )
-    email = db.Column( db.String( 120 ) )
 
-    def __init__( self, name, email ):
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+   
+    
+    def __init__(self, name, email):
         self.name = name
         self.email = email
-
-    def __repr__( self ):
-        return '<Name %r>' % self.name # for compatibility with sqlalchemy
-     
-    def serialize( self ):
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+    @property
+    def username(self):
+        return self.name
+    
+    @username.setter
+    def username(self, name):
+        self.name = name
+        
+    def to_dict(self):
         return {
-            'id': self.id,
             'name': self.name,
             'email': self.email
         }
-          
         
     
-     
+    
+      
